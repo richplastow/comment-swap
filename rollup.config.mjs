8bdb4c5d6325133ff6619d42c8126cc5ea52e9be
Rich Plastow <rich@richplastow.com>
2022-12-29 12:26:47 +0000

    adds rollup.config.mjs, based on the Rollup plugins repo

---
+++ rollup.config.mjs
@@ -0,0 +1,45 @@
+// Configuration, used by `rollup -c` during `npm run build`.
+
+import { readFileSync } from 'fs';
+import typescript from 'rollup-plugin-typescript2';
+
+const pkg = JSON.parse(
+    readFileSync(new URL('./package.json', import.meta.url), 'utf8')
+);
+
+export default {
+    input: 'src/index.ts',
+    output: [
+      {
+        format: 'cjs',
+        file: pkg.main,
+        exports: 'named',
+        footer: 'module.exports = Object.assign(exports.default, exports);',
+        sourcemap: true
+      },
+      {
+        format: 'es',
+        file: pkg.module,
+        plugins: [
+          emitModulePackageFile()
+        ],
+        sourcemap: true
+      },
+    ],
+    plugins: [
+      typescript({ sourceMap: true }),
+    ],
+};
+
+function emitModulePackageFile() {
+    return {
+        name: 'emit-module-package-file',
+        generateBundle() {
+            this.emitFile({
+                type: 'asset',
+                fileName: 'package.json',
+                source: '{"type":"module"}',
+            });
+        }
+    };
+}
